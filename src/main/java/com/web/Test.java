33e673aad9592ef9eb72b7119394575215451dac
liyangjin <772013350@qq.com>
2025-05-16 17:27:58 +0800

    .

--- src/main/java/com/web/Test.java
+++ src/main/java/com/web/Test.java
@@ -8,7 +8,9 @@ package com.web;
 public class Test {
 
     public static void main(String[] args) {
-        calculate();
+        String line = "OAID_84467FEA0A28B4A2C355DC599AFDE419   2       f82d8660-1d68-4401-902a-84dc4210d59e    HUAWEI  1747230317";
+        String[] parts = line.trim().split("\\s+");
+        System.out.println("parts.length=" + parts.length);
     }
 
     /**
